6985dd1d2cf5414f97dea0144df7ceb2b4ef2c9e
JP <juanpimod@gmail.com>
2025-07-10 13:12:03 -0600

    GET endpoint does work. We gotta solve POST.

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,37 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import json
+import datetime
+from peewee import MySQLDatabase, Model, CharField, TextField, DateTimeField
+from playhouse.shortcuts import model_to_dict
+from pathlib import Path
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
+
 
 load_dotenv()
 app = Flask(__name__)
 
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+              
 @app.route('/')
 def index():
     json_path_edu = os.path.join(app.root_path,"data", "education.json")
@@ -26,3 +53,26 @@ def hobbies():
 @app.route('/map')
 def map():
     return render_template('map.html', title="Places I've been to", url=os.getenv("URL"))
+
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_timelinePost():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_timelinePost():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in 
+TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+if __name__ == "__main__":
+    print(f"Server up. \n MySQL running:{mydb}")

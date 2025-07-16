0a5e08358fdd15619b5009abad7ea64017a0a2f5
Arianna <44093846+starry-ari@users.noreply.github.com>
2025-07-16 16:03:15 -0400

    tests

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import json
 import datetime
-from peewee import MySQLDatabase, Model, CharField, TextField, DateTimeField
+from peewee import *
 from playhouse.shortcuts import model_to_dict
 from pathlib import Path
 load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
@@ -12,13 +12,20 @@ load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
 load_dotenv()
 app = Flask(__name__)
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306
-)
+
+
+
+if os.getenv("TESTING")=="true":
+    print("Running in test mode")
+    mydb= SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
+    
 
 class TimelinePost(Model):
     name = CharField()
@@ -62,6 +69,13 @@ def post_timelinePost():
     name = request.form['name']
     email = request.form['email']
     content = request.form['content']
+    if not name:
+        return "Invalid name", 400
+    if '@' not in email:
+        return "Invalid email", 400
+    if not content:
+        return "Invalid content", 400
+
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 

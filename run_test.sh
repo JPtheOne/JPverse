b537281ceebdb0946903f262e1bb23b81531f236
JPtheOne <juanpimod@gmail.com>
2025-08-04 12:53:49 -0600

    Corrected simple py test.

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Simple test runner without Docker (for GitHub Actions exercise)
-
-python -m unittest discover -s tests -v
+./python3-virtualenv/bin/python -m unittest discover -s tests -v
